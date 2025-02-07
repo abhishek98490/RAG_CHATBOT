55b32d771ce800f3cee5f009a4a55fa3ae385386
abhishek <sharmaabhi.98490@gmail.com>
2025-02-07 20:55:54 +0530

    updated retrival.py with retrival strategy

--- src/vector_database/retrival.py
+++ src/vector_database/retrival.py
@@ -34,19 +34,60 @@ class Chroma_database():
             return
         batch_size = 200
         logging.info(f"adding data to the chromadb collection")
-        for i in range(0, len(texts), batch_size):
-            end_idx = min(i + batch_size, len(texts))
-            self.collection.add(
-                documents=texts[i:end_idx],
-                metadatas=metadatas[i:end_idx],
-                ids=ids[i:end_idx]
-            )
+        try :
+            for i in range(0, len(texts), batch_size):
+                end_idx = min(i + batch_size, len(texts))
+                self.collection.add(
+                    documents=texts[i:end_idx],
+                    metadatas=metadatas[i:end_idx],
+                    ids=ids[i:end_idx]
+                )
+            logging.info(f"data added without any errors")
+        except Exception as e:
+            logging.info(f"Error adding {filename}: {str(e)}")
+            raise customexception(e,sys)
     
     def process_and_add_documents(self,chunks, filename: str):
         ids, texts, metadatas = self.data_pre_processing(chunks,filename)
         self.add_to_collection(ids, texts, metadatas)
         
         print(f"Added {len(texts)} chunks to collection")
+    
+    def formated_context_with_sources(self,results):
+        try:
+            context = results[0]['documents']
+            for i in range(1,len(results)):
+                context += "\n\n"+ results[i]["documents"]
+            sources = [f"{i['metadatas']['source']}(chunk {i['metadatas']['chunk']})" 
+                    for i in results]
+
+            return context, sources
+        except Exception as e:
+            logging.info(f"Error Formating {filename}: {str(e)}")
+            raise customexception(e,sys)
+    
+    def retrive_text(self, query: str, filename:str, n_results: int = 2):
+        try:
+            results = self.collection.query(
+                query_texts=[query],
+                n_results=n_results
+            )
+            logging.info(f"data retrived from chromadb")
+            if file_name:
+                filtered_text = []
+                for i in range(n_results):
+                    if results['metadatas'][0][i]["source"] == filename:
+                        filtered_text.append({"ids": results['ids'][0][i],
+                                                "documents": results["documents"][0][i],
+                                                "metadatas": results['metadatas'][0][i],
+                                                "distances": results['distances'][0][i]
+                                                })
+            context, sources = formated_context_with_sources(filtered_text)
+            logging.info(f"data formated")
+            return context, sources
+        except Exception as e:
+            logging.info(f"Error retriving {filename}: {str(e)}")
+            raise customexception(e,sys)
         
 if __name__=="__main__":
     chunks = ["Greeting everyone. Today, I am here to deliver a speech on APJ Abdul Kalam. Dr APJ Abdul Kalam’s full name was Avul Pakir Zainuldeben Abdul Kalam, very few people know him by his full name as he was mostly addressed as ‘Missile Man of India’ and ‘People’s President’. He was born into a very poor family in Rameswaram on October 15, 1931. Since childhood, he enjoyed flying, and was equally curious to know how birds fly in the air? He was very intelligent and enjoyed reading, but his family did not have sufficient income for his school fees, so to support his education, he would wake up early in the morning and ride a bicycle 3 kilometres from home to collect newspapers and sell them. He was admitted to St. Joseph's College, Tiruchirapalli, and later he went on to complete a degree in physics in 1954 and then studied at the Madras Institute of Technology and graduated in aeronautical engineering in 1955. Since his childhood, Dr Abdul Alam wanted to be a pilot but couldn’t make his dream come true. He learned from his mistakes and accomplished numerous achievements in his life. After completing his degree, Abdul Kalam entered the",
@@ -56,3 +97,6 @@ if __name__=="__main__":
                 "the defence technology of India and his outstanding contribution. From July 25, 2002 - July 25, 2007, he served as President of India, becoming famous among Indians and receiving a lot of attention from Indian youth. He became popular as the People's President. Kalam worked as a professor, chancellor, and assistant at many institutions after leaving office. He experienced serious cardiac arrest on the evening of July 27, 2015, and fell unconscious and died 2 hours later. In 1999, Kalam published his autobiography and a book called The Wings of Fire. He has written many other books that are useful to the people of every generation."]
     db = Chroma_database()
     db.process_and_add_documents(chunks,"text.txt")
+    query = "When did Dr. APJ Abdul Kalam pass away, and how?"
+    results = db.retrive_text(query, file_name="text.txt",n_results=2)
+    results
